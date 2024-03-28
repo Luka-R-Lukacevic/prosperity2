ded2bfb464f0703209296435cbc77b98e4aa83a7
unknown <file.acomplaint.contact@gmail.com>
2024-03-28 21:04:31 +0100

    Backtester repariert

---
+++ testing/MarketMaking.py
@@ -0,0 +1,143 @@
+from datamodel import OrderDepth, UserId, TradingState, Order
+from typing import List
+import string
+
+import json
+from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
+from typing import Any
+
+class Logger:
+    def __init__(self) -> None:
+        self.logs = ""
+
+    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
+        self.logs += sep.join(map(str, objects)) + end
+
+    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]], conversions: int, trader_data: str) -> None:
+        print(json.dumps([
+            self.compress_state(state),
+            self.compress_orders(orders),
+            conversions,
+            trader_data,
+            self.logs,
+        ], cls=ProsperityEncoder, separators=(",", ":")))
+
+        self.logs = ""
+
+    def compress_state(self, state: TradingState) -> list[Any]:
+        return [
+            state.timestamp
+        ]
+
+    def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
+        compressed = []
+        for listing in listings.values():
+            compressed.append([listing["symbol"], listing["product"], listing["denomination"]])
+
+        return compressed
+
+    def compress_order_depths(self, order_depths: dict[Symbol, OrderDepth]) -> dict[Symbol, list[Any]]:
+        compressed = {}
+        for symbol, order_depth in order_depths.items():
+            compressed[symbol] = [order_depth.buy_orders, order_depth.sell_orders]
+
+        return compressed
+
+    def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
+        compressed = []
+        for arr in trades.values():
+            for trade in arr:
+                compressed.append([
+                    trade.symbol,
+                    trade.price,
+                    trade.quantity,
+                    trade.buyer,
+                    trade.seller,
+                    trade.timestamp,
+                ])
+
+        return compressed
+
+    def compress_observations(self, observations: Observation) -> list[Any]:
+        conversion_observations = {}
+        for product, observation in observations.conversionObservations.items():
+            conversion_observations[product] = [
+                observation.bidPrice,
+                observation.askPrice,
+                observation.transportFees,
+                observation.exportTariff,
+                observation.importTariff,
+                observation.sunlight,
+                observation.humidity,
+            ]
+
+        return [observations.plainValueObservations, conversion_observations]
+
+    def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
+        compressed = []
+        for arr in orders.values():
+            for order in arr:
+                compressed.append([order.symbol, order.price, order.quantity])
+
+        return compressed
+
+logger = Logger()
+
+Limits = {"AMETHYSTS" : 20, "STARFRUIT" : 20}
+
+class Trader:
+    def __init__(self):
+        self.last5k = {key: [] for key in Limits}
+
+    def run(self, state: TradingState) -> tuple[dict[Symbol, list[Order]], int, str]:
+        conversions = 0
+        trader_data = ""
+
+        logger.print("Observations: " + str(state.observations))
+        result = {}
+        for product in state.listings:
+            if product not in state.market_trades.keys():
+                continue
+            if product not in Limits:
+                continue
+            Limit = Limits[product]
+            trades: List[Trade] = state.market_trades[product]
+            logger.print("Number of Trades : " + str(len(trades)))
+            orders: List[Order] = []
+
+            for trade in trades:
+                self.last5k[product].append(trade)
+
+            volume = 0
+            price = 0
+            for trade in self.last5k[product]:
+                if trade.timestamp + 5000 < state.timestamp:
+                    self.last5k[product].remove(trade)        
+                volume += abs(trade.quantity)
+                price += abs(trade.quantity) * trade.price
+            estimated_fair_value = price / volume
+
+            logger.print("Fair price : " + str(estimated_fair_value))
+    
+            bid = int (estimated_fair_value) -3
+            ask = bid+6
+
+            if (state.position.get(product, 0) / Limit > 0.5):
+                bid -= 2
+                ask -= 2
+            if (state.position.get(product, 0) / Limit < -0.5):
+                bid += 2
+                ask += 2
+                    
+            orders.append(Order(product, bid, Limit - state.position.get(product, 0)))
+            orders.append(Order(product, ask, - Limit - state.position.get(product, 0)))
+            
+            result[product] = orders
+    
+    
+        trader_data = "SAMPLE" # String value holding Trader state data required. It will be delivered as TradingState.traderData on next execution.
+        
+        #conversions = 100
+
+        logger.flush(state, result, conversions, trader_data)
+        return result, conversions, trader_data
