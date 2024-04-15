58631ba64ad92b07783c5079321d8bbb05ad1636
Luka-R-Lukacevic <luca.lukacevic@gmail.com>
2024-04-15 16:39:39 +0200

    add some data analysis for basket

--- round_02/databottle/Data analysis_Humidity_Sunlight.R
+++ round_02/databottle/Data analysis_Humidity_Sunlight.R
@@ -6,44 +6,58 @@ prices_neg_1 <-read.csv(path_prices_neg_1, sep=";")
 prices_0 <-read.csv(path_prices_0, sep=";")
 prices_pos_1 <-read.csv(path_prices_pos_1, sep=";")
 
-#bind into 2 dfs 
+#bind into 1 df 
 prices <- rbind(prices_neg_1, prices_0, prices_pos_1)
 
+library(ggplot2)
 
-y <- prices$ORCHIDS
-sunl <- prices$SUNLIGHT
-hum <- prices$HUMIDITY
-transport <- prices$TRANSPORT_FEES
-import <- prices$IMPORT_TARIFF
-export <- prices$EXPORT_TARIFF
-n <- length(y)
+# Define the variables to plot
+variables <- c("ORCHIDS", "TRANSPORT_FEES", "EXPORT_TARIFF", "IMPORT_TARIFF", "SUNLIGHT", "HUMIDITY")
 
-prod_sunl <- function(sl){
-  if(sl < 2916){
-    return(-0.04*(2916-sl)/69.4)
-  }
-  return(0)
+# Loop over each variable and create a histogram
+plots <- lapply(variables, function(var) {
+  ggplot(prices, aes_string(x = var, fill = "factor(DAY)")) +  # aes_string allows using string input for aes
+    geom_histogram(position = "identity", alpha = 0.5, bins = 30) +
+    ggtitle(paste("Histogram of", var)) +
+    xlab(var) +
+    ylab("Frequency") +
+    scale_fill_manual(values = c("-1" = "red", "0" = "green", "1" = "blue"), name = "Day") +
+    theme_minimal()
+})
+
+# Print all plots
+lapply(plots, print)
+
+
+create_time_series_plot <- function(var) {
+  ggplot(prices, aes(x = timestamp, y = .data[[var]], color = as.factor(DAY))) +
+    geom_line() +  # Use geom_line for time series
+    ggtitle(paste("Time Series of", var, "by Day")) +
+    xlab("Timestamp") +
+    ylab(var) +
+    scale_color_manual(values = c("-1" = "red", "0" = "green", "1" = "blue"), name = "Day") +
+    theme_minimal()
 }
 
-prod_hum <- function(hm){
-  if(hm > 80){
-    return(-0.02*(hm-80)/5)
-  }
-  if(hm < 60){
-    return(0.02*(hm-60)/5)
-  }
-  return(0)
-}
+# Generate and print plots for each variable
+plots <- lapply(variables, create_time_series_plot)
+lapply(plots, print)
 
-for (i in 2:n) {
-  production[i] = 1 + prod_sunl(sunl[i]) + prod_hum(hum[i])
-}
+library(dplyr)
+prices$DAY <- as.factor(prices$DAY)  # Ensure DAY is a factor
+results <- lapply(variables, function(var) {
+  kruskal.test(reformulate("DAY", response = var), data = prices)
+})
 
-plot(production, type = 'l')
+# Print results
+names(results) <- variables
+results
 
-n <- length(y)
-offset <- as.numeric(10)
-y_shift <- y[offset:n]
-length(y_shift)
-model0 <- lm(y_shift~production[0:(-offset+1)]+sunl[0:(-offset+1)]+hum[0:(-offset+1)]+transport[0:(-offset+1)]+export[0:(-offset+1)]+import[0:(-offset+1)])
-summary(model0)
+
+# Assuming prices$DAY is a factor with levels indicating days
+day_minus_one <- prices$IMPORT_TARIFF[prices$DAY == "-1"]
+day_zero <- prices$IMPORT_TARIFF[prices$DAY == "0"]
+
+# Perform the two-sample KS test
+ks_test_result <- ks.test(day_minus_one, day_zero)
+print(ks_test_result)
